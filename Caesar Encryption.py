4ec53393de0ad227592b3a49612fce4d609f6158
Mehmet Sefa Ercan <sefaercane@gmail.com>
2020-08-28 23:57:03 +0300

    Update Caesar Encryption.py

--- Caesar Encryption.py
+++ Caesar Encryption.py
@@ -1,17 +1,15 @@
 def sezar(yazi,birim):
     yaziBüyük = yazi.upper()
-    sifreliYazi2 = []
     sifreliYazi = []
     alfabe = ["A", "B", "C","Ç","D", "E", "F", "G","Ğ", "H", "I","İ", "J", "K", "L", "M", "N", "O","Ö", "P","R", "S","Ş", "T","U","Ü" ,"V", "Y", "Z"]
 
     for i in yaziBüyük:
         harfSira  = alfabe.index(i)
         sifreliHarf = (harfSira + birim) % 29
-        sifreliYazi.append(sifreliHarf)
         cikti = alfabe[sifreliHarf]
-        sifreliYazi2.append(cikti)
+        sifreliYazi.append(cikti)
 
     yazihali = ""
-    for i in sifreliYazi2:
+    for i in sifreliYazi:
         yazihali = yazihali + i
     return yazihali.lower()
